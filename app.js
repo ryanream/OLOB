b63e412eb7177c02b7f8865fe9c997bf1d6a9ef8
Ryan Ream <ryan.a.ream@gmail.com>
2019-02-21 20:25:58 +0000

    changed DB location for deploy to Heroku, cross your fingers

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ var express     = require("express"),
     mongoose    = require("mongoose"),
     Post        = require("./models/post.js"),
     bodyParser  = require("body-parser");
+    
+// mongodb+srv://ryanream:<PASSWORD>@cluster0-mqbh5.mongodb.net/test?retryWrites=true
+// mongoose.connect("mongodb+srv://ryanream:<mongopassword>@cluster0-mqbh5.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
 
-mongoose.connect("mongodb://localhost/olob_blog", {useNewUrlParser: true});
+    var MongoClient = require('mongodb').MongoClient;
+    // var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+    var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,cluster0-shard-00-01-mqbh5.mongodb.net:27017,cluster0-shard-00-02-mqbh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+    MongoClient.connect(uri, {useNewUrlParser: true}, function(err, db) {
+        if(err) {
+            console.log(err);
+        }
+    });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
