b9b7b6c1c632368950e709038e4f90853cb9c356
Ryan Ream <ryan.a.ream@gmail.com>
2019-02-22 07:58:01 +0000

    change color scheme, wording, fonts, styling, tested image uploads

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ var express     = require("express"),
     Post        = require("./models/post.js"),
     bodyParser  = require("body-parser");
     
+    mongoose.connect("mongodb://localhost/olob_blog", {useNewUrlParser: true});
+
 // mongodb+srv://ryanream:<PASSWORD>@cluster0-mqbh5.mongodb.net/test?retryWrites=true
 // mongoose.connect("mongodb+srv://ryanream:<mongopassword>@cluster0-mqbh5.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
-
-    var MongoClient = require('mongodb').MongoClient;
     // var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
-    var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,cluster0-shard-00-01-mqbh5.mongodb.net:27017,cluster0-shard-00-02-mqbh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-    MongoClient.connect(uri, {useNewUrlParser: true}, function(err, db) {
-        if(err) {
-            console.log(err);
-        }
-    });
+
+    // var MongoClient = require('mongodb').MongoClient;
+    // // var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,cluster0-shard-00-01-mqbh5.mongodb.net:27017,cluster0-shard-00-02-mqbh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+    // var uri = "mongodb+srv://ryanream:mongopassword@cluster0-mqbh5.mongodb.net/test?retryWrites=true";
+    // MongoClient.connect(uri, {useNewUrlParser: true}, function(err, db) {
+    //     if(err) {
+    //         console.log(err);
+    //     }
+    // });    
+    
+    // var MongoClient = require('mongodb').MongoClient;
+    // var uri = "mongo "mongodb+srv://cluster0-mqbh5.mongodb.net/test";
+    // MongoClient.connect(uri, {useNewUrlParser: true}, function(err, db) {
+    //     if(err) {
+    //         console.log(err);
+    //     }
+    // });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
@@ -47,17 +58,16 @@ app.post("/new", function(req, res){
     var newTitle = req.body.title;
     var newBody = req.body.body;
     var newName = req.body.name;
+    var newImage = req.body.image;
+    var newPhoto = req.body.photo;
     // add to posts array
-    var newPost = {title: newTitle, body: newBody, name: newName};
-    
+    var newPost = {title: newTitle, body: newBody, name: newName, image: newImage, photo: newPhoto};
+
     Post.create(newPost, function(err, newlyCreated){
         if (err) {
             console.log(err);
         } else {
-            console.log("title - " + newTitle);
-            console.log("body - " + newBody);
-            console.log("name - " + newName);
-            console.log(req.body.image);
+            console.log("New post by - " + newName);
             res.redirect("/");
         }
     });
--- models/post.js
+++ models/post.js
@@ -1,12 +1,27 @@
 var mongoose    = require("mongoose");
-mongoose.connect("mongodb://localhost/olob_blog", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost/olob_blog", {useNewUrlParser: true});
+    // var MongoClient = require('mongodb').MongoClient;
+    // var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,cluster0-shard-00-01-mqbh5.mongodb.net:27017,cluster0-shard-00-02-mqbh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+    // MongoClient.connect(uri, {useNewUrlParser: true}, function(err, db) {
+    //     if(err) {
+    //         console.log(err);
+    //     }
+    // }); 
+    // var MongoClient = require('mongodb').MongoClient;
+    // // var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+    // var uri = "mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,cluster0-shard-00-01-mqbh5.mongodb.net:27017,cluster0-shard-00-02-mqbh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+    // MongoClient.connect(uri, {useNewUrlParser: true}, function(err, db) {
+    //     if(err) {
+    //         console.log(err);
+    //     }
+    // });
 
 // Create the post Schema
 var postSchema = new mongoose.Schema({
     title: String,
     body: String,
     name: String,
-    image: String
+    image: String,
 });
 
 // Compile into a model (using above Schema) that allows the use of methods (.find(), .create())
