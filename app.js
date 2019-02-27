f173104d7cd0fc4f9578c2ff4958976fbf5ac105
Ryan Ream <ryan.a.ream@gmail.com>
2019-02-27 21:10:55 +0000

    change db string for heroku

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     methodOverride = require("method-override");
     
-    mongoose.connect("mongodb://localhost/olob_blog", {useNewUrlParser: true});
-    // mongoose.connect("mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,cluster0-shard-00-01-mqbh5.mongodb.net:27017,cluster0-shard-00-02-mqbh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", {useNewUrlParser: true});
+    // mongoose.connect("mongodb://localhost/olob_blog", {useNewUrlParser: true});
+    mongoose.connect("mongodb://ryanream:mongopassword@cluster0-shard-00-00-mqbh5.mongodb.net:27017,cluster0-shard-00-01-mqbh5.mongodb.net:27017,cluster0-shard-00-02-mqbh5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
